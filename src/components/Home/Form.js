2d4a471e4388e4ced4c34d75e241c3e14098440e
borshon420 <borshonarafat420@gmail.com>
2021-12-17 02:34:18 +0600

    added country details

--- src/components/Home/Form.js
+++ src/components/Home/Form.js
@@ -1,17 +1,30 @@
 import React, { useState } from 'react';
 
 const Form = () => {
-    const [country, setCountry] = useState("");
+    const [name, setName] = useState("");
+    const [country, setCountry] = useState([]);
 
+    
     const handleOnChange = e => {
-        setCountry(e.target.value);
-        console.log(country)
+        setName(e.target.value);
+    }
+
+    const loadCountry = () => {
+        fetch(`https://restcountries.com/v3.1/name/${name}`)
+        .then(res => res.json())
+        .then(data => setCountry(data))
     }
 
     const handleSubmit = e => {
-        e.prevent.default();
+        e.preventDefault();
+        loadCountry();          
     }
+    console.log(country[0].capital[0])
+    console.log(country[0].population)
+    console.log(country[0].latlng)
+    console.log(country[0].flags.svg)
     return (
+        <>
         <div>
             <h1>This is form</h1>
             <form onSubmit={handleSubmit}>
@@ -19,9 +32,18 @@ const Form = () => {
                 type="text" 
                 onChange={handleOnChange}
                 placeholder='Enter Country'/>
-                {country?<button type='submit'>Submit</button>: <button type='submit' disabled>Submit</button>}
+                {name?<button type='submit' >Submit</button>: <button type='submit' disabled>Submit</button>}
             </form>
         </div>
+        <div>
+            {name?<div>
+                <p>capital: {country[0].capital[0]} </p>
+            <p>Population: {country[0].population} </p>
+            <p>Latlang: {country[0].latlng[0]}</p>
+            <img src={country[0].flags.svg} alt="" />
+            </div>: <div></div>}
+        </div>
+        </>
     );
 };
 
